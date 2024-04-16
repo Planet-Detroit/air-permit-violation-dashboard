139d078515726330429c250ed092fc9878d249bf
srjouppi <90423448+srjouppi@users.noreply.github.com>
2024-04-16 12:06:05 -0600

    adding google sheets publishing script

---
+++ sheets-updater.py
@@ -0,0 +1,31 @@
+# Importing required libraries
+import pygsheets
+from pygsheets.datarange import DataRange
+import pandas as pd
+  
+# Create the cietn and enter 
+gc = pygsheets.authorize(service_account_env_var = 'PYG_SHEETS_AUTH')
+
+# Grab the spreadsheet
+sh = gc.open('EGLE-AQD-Violation-Notices-2018-Present')
+
+# Grab the worksheet
+wks = sh.worksheet_by_title("data")
+
+# Define a dataframe
+df = pd.read_csv('output/EGLE-AQD-Violation-Notices-2018-Present.csv')
+
+# Assign the dataframe to the worksheet
+wks.set_dataframe(df, start='A1', fit=True)
+
+# Freeze the first row
+wks.frozen_rows=1
+
+# Bold the first row
+model_cell = wks.cell('A1')
+
+model_cell.set_text_format('bold', True)
+
+drange = pygsheets.datarange.DataRange(start='A1', end='T1', worksheet=wks)
+
+drange.apply_format(model_cell)
